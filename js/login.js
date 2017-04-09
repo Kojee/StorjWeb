5b7dd2b03a18e4caf7ac00fa996cc1ade98ea2d3
MarcoFilippozzi <marco.filippozzi98@gmail.com>
2017-04-09 18:32:23 +0200

    Ora si riesce a creare un bucket, ma solo loggando con una private key. E' ancora da capire come mai usando email e password non si possa.

--- js/login.js
+++ js/login.js
@@ -1,20 +1,57 @@
 function Login(){
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
 
-    var basicAuth = {
-        username: document.getElementById("username").value,
-        password: document.getElementById("password").value
-    }
-    var options = {
-        basicAuth: basicAuth
+    var storj = new Storj()
+
+    var opts = {
+        basicAuth: {
+            email: document.getElementById("username").value,
+            password: document.getElementById("password").value
+        }
+    };
+    console.log(opts.basicAuth.username);
+    console.log(opts.basicAuth.password);
+    var storj = new Storj(opts);
+
+    // Create a new keypair
+    var keypair = storj.generateKeyPair();
+    var privateKey = keypair.getPrivateKey();
+    var publicKey = keypair.getPublicKey();
+    document.getElementById("privateKeyLabel").innerHTML = privateKey;
+    // Register the public key to your account
+    storj.registerKey(publicKey, function (error) {
+    if(error) {
+        return console.log(error)
+    }});
+
+    // Authenticate with the keypair
+    var opts = {
+    key: privateKey
     }
 
-    var storj = new Storj(options);
-    storj.on('error', function (e) {
-        console.log("error");
+    storj = new Storj(opts)
+
+    // Create a bucket
+    storj.createBucket("My New Bucket", function (error, meta){
+    if (error) {
+        return console.log(error)
+    }
+    console.log("id:", meta.id)
+    console.log("name:", meta.name)
     });
-    storj.on('ready', function () {
-        console.log("ready");
-    });
+}
+
+function LoginK(){
+    var keyOptions = {
+        key: document.getElementById("privateKey").value
+    };
+
+    var storjK = new Storj(keyOptions);
+    
+    storjK.createBucket("My New Bucket", function (error, meta){
+        if (error) {
+            return console.log(error)
+        }
+        console.log("id:", meta.id)
+        console.log("name:", meta.name)
+        });
 }
