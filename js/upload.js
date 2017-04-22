8ee6380027d14efd3d1c9690484aab7a3dc53297
Kojee <marco.filippozzi98@gmail.com>
2017-04-22 18:42:05 +0200

    refined file uploading

--- js/upload.js
+++ js/upload.js
@@ -11,3 +11,24 @@ function getStorj(){
     var bucketId;
     return storj;
 }
+
+function upload(bucketId){
+    var opts = {
+        basicAuth: {
+            email: localStorage.getItem("email"),
+            password: localStorage.getItem("password")
+        },
+        encryptionKey : Storj.generateEncryptionKey()
+    };
+    
+    var storj = new Storj(opts);
+    var data = document.getElementById('input').files[0]
+    var file = storj.createFile(bucketId, data.name, data)
+    file.on('error', console.log)
+    file.on('done', function() {
+        console.log('Created file with id: ' + file.id)
+        location.reload();
+    })
+
+    
+}
